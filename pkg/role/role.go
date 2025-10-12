3a0a356d7f472faaa104278bd50f30a93f6ab60a
Mickaël Carl <contact@carlm.org>
2025-10-12 22:32:11 +0100

    Wire in variables support in roles
    This should support inventory variables overriding role variables
    themselves overriding role defaults.

--- pkg/role/role.go
+++ pkg/role/role.go
@@ -2,8 +2,10 @@ package role
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/fs"
+	"maps"
 	"os"
 	"path"
 
@@ -44,14 +46,67 @@ func DiscoverRoles(ctx context.Context, rolesPath string) (map[string]Role, erro
 	return roles, nil
 }
 
+// maybeRole checks if a given directory is indeed an Ansible role or not by
+// checking for known directories that are a role may contain. It does so in
+// the order required to preserve variables precedence rules.
 func maybeRole(ctx context.Context, fsys fs.FS, name string) (Role, bool, error) {
+	isARole := false
+	var role Role
+
+	// First check for defaults, since those have low precedence.
+	_, err := fs.Stat(fsys, path.Join(name, "defaults"))
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return Role{}, false, err
+		}
+	} else {
+		isARole = true
+		defaults, err := processVars(fsys, path.Join(name, "defaults"))
+		if err != nil {
+			return Role{}, false, err
+		}
+		role.Defaults = defaults
+	}
+
+	// Then check for role variables, since those have higher precedence than
+	// defaults, but lower than play/inventory-level variables.
+	// First check for defaults, since those have low precedence.
+	_, err = fs.Stat(fsys, path.Join(name, "vars"))
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return Role{}, false, err
+		}
+	} else {
+		isARole = true
+		vars, err := processVars(fsys, path.Join(name, "vars"))
+		if err != nil {
+			return Role{}, false, err
+		}
+		role.Variables = vars
+	}
+
+	// Per https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_variables.html#understanding-variable-precedence:
+	// defaults have lowest precedence. Over that are inventory vars, then
+	// playbook vars, then role vars.
+	roleVars := inventory.Variables{}
+	maps.Copy(roleVars, role.Defaults)
+
+	additionalVars, ok := ctx.Value("vars").(inventory.Variables)
+	if !ok {
+		additionalVars = inventory.Variables{}
+	}
+	maps.Copy(roleVars, additionalVars)
+
+	maps.Copy(roleVars, role.Variables)
+
+	roleCtx := context.WithValue(ctx, "vars", roleVars)
+
+	// Then process the rest of the directory (order doesn't really matter anymore).
 	entries, err := fs.ReadDir(fsys, name)
 	if err != nil {
 		return Role{}, false, err
 	}
 
-	isARole := false
-	var role Role
 	for _, entry := range entries {
 		if !entry.IsDir() {
 			continue
@@ -63,25 +118,9 @@ func maybeRole(ctx context.Context, fsys fs.FS, name string) (Role, bool, error)
 		// below is *populated*. We only check for the directory's existence
 		// instead. This may introduce a deviation in behaviour.
 		switch entry.Name() {
-		case "defaults":
-			isARole = true
-			defaults, err := processVars(fsys, entryPath)
-			if err != nil {
-				return Role{}, false, err
-			}
-			role.Defaults = defaults
-
-		case "vars":
-			isARole = true
-			vars, err := processVars(fsys, entryPath)
-			if err != nil {
-				return Role{}, false, err
-			}
-			role.Variables = vars
-
 		case "tasks":
 			isARole = true
-			tasks, err := processTasks(ctx, fsys, entryPath)
+			tasks, err := processTasks(roleCtx, fsys, entryPath)
 			if err != nil {
 				return Role{}, false, err
 			}
@@ -91,7 +130,6 @@ func maybeRole(ctx context.Context, fsys fs.FS, name string) (Role, bool, error)
 		case "handlers", "templates", "files", "meta", "library", "module_utils", "lookup_plugins":
 			isARole = true
 		default:
-			isARole = false
 		}
 	}
 
