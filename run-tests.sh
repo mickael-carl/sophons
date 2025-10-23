0269a162b19a487adb6d1f5363267b0967de7b4d
Mickaël Carl <contact@carlm.org>
2025-10-23 09:20:52 +0100

    Fix ssh-keyscan being spammy for real
    Also `-H` does not mean `host`, but `hash`. Not what we want.

--- run-tests.sh
+++ run-tests.sh
@@ -16,7 +16,7 @@ touch "${KNOWN_HOSTS}"
 
 setup_known_hosts() {
     if ! grep -q "\[${HOST}\]:${PORT}" "${KNOWN_HOSTS}" 2>/dev/null; then
-        ssh-keyscan -p "${PORT}" -H "${HOST}" | sed "s/^${HOST}/[${HOST}]:${PORT}/" >> "${KNOWN_HOSTS}" 2>/dev/null
+        ssh-keyscan -p "${PORT}" "${HOST}" 2> /dev/null | sed "s/^${HOST}/[${HOST}]:${PORT}/" >> "${KNOWN_HOSTS}"
     fi
 }
 
