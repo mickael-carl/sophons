d5628baf5770bf9694342d408c62595ee0a00a6e
Mickaël Carl <contact@carlm.org>
2025-12-02 22:47:49 +0000

    Move ansible.builtin.copy to use the protobuf generated definition

--- pkg/exec/copy_test.go
+++ pkg/exec/copy_test.go
@@ -1,14 +1,20 @@
 package exec
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/mickael-carl/sophons/pkg/proto"
+)
 
 func TestCopyValidate(t *testing.T) {
 	tests := []ValidationTestCase[*Copy]{
 		{
 			Name: "absolute path without remote_src",
 			Input: &Copy{
-				Src:  "/etc/shadow",
-				Dest: "/hacking-passwords",
+				Copy: proto.Copy{
+					Src:  "/etc/shadow",
+					Dest: "/hacking-passwords",
+				},
 			},
 			WantErr: true,
 			ErrMsg:  "copying from an absolute path without remote_src is not supported",
@@ -16,16 +22,20 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "absolute path with remote_src",
 			Input: &Copy{
-				Src:       "/tmp/someconfig",
-				Dest:      "/etc/someconfig",
-				RemoteSrc: true,
+				Copy: proto.Copy{
+					Src:       "/tmp/someconfig",
+					Dest:      "/etc/someconfig",
+					RemoteSrc: true,
+				},
 			},
 			WantErr: false,
 		},
 		{
 			Name: "missing src and content",
 			Input: &Copy{
-				Dest: "/something",
+				Copy: proto.Copy{
+					Dest: "/something",
+				},
 			},
 			WantErr: true,
 			ErrMsg:  "either src or content need to be specified",
@@ -33,8 +43,10 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "content with directory dest",
 			Input: &Copy{
-				Content: "hello world",
-				Dest:    "/some/directory/",
+				Copy: proto.Copy{
+					Content: "hello world",
+					Dest:    "/some/directory/",
+				},
 			},
 			WantErr: true,
 			ErrMsg:  "can't use content when dest is a directory",
@@ -42,9 +54,11 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "both src and content set",
 			Input: &Copy{
-				Src:     "somefile",
-				Content: "hello world!",
-				Dest:    "/somefile",
+				Copy: proto.Copy{
+					Src:     "somefile",
+					Content: "hello world!",
+					Dest:    "/somefile",
+				},
 			},
 			WantErr: true,
 			ErrMsg:  "src and content can't both be specified",
@@ -52,8 +66,10 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "empty src",
 			Input: &Copy{
-				Src:  "",
-				Dest: "/somewhere",
+				Copy: proto.Copy{
+					Src:  "",
+					Dest: "/somewhere",
+				},
 			},
 			WantErr:     true,
 			ErrContains: "src",
@@ -61,24 +77,30 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "valid relative src",
 			Input: &Copy{
-				Src:  "files/config.txt",
-				Dest: "/etc/config.txt",
+				Copy: proto.Copy{
+					Src:  "files/config.txt",
+					Dest: "/etc/config.txt",
+				},
 			},
 			WantErr: false,
 		},
 		{
 			Name: "content with newlines",
 			Input: &Copy{
-				Content: "line1\nline2\nline3",
-				Dest:    "/tmp/multiline.txt",
+				Copy: proto.Copy{
+					Content: "line1\nline2\nline3",
+					Dest:    "/tmp/multiline.txt",
+				},
 			},
 			WantErr: false,
 		},
 		{
 			Name: "empty content still requires src",
 			Input: &Copy{
-				Content: "",
-				Dest:    "/tmp/empty.txt",
+				Copy: proto.Copy{
+					Content: "",
+					Dest:    "/tmp/empty.txt",
+				},
 			},
 			WantErr:     true,
 			ErrContains: "src or content",
@@ -86,8 +108,10 @@ func TestCopyValidate(t *testing.T) {
 		{
 			Name: "dest with trailing slash is directory",
 			Input: &Copy{
-				Src:  "file.txt",
-				Dest: "/tmp/dir/",
+				Copy: proto.Copy{
+					Src:  "file.txt",
+					Dest: "/tmp/dir/",
+				},
 			},
 			WantErr: false,
 		},
